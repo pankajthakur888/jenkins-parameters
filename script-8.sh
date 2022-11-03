7438ece205e7b5109b70de42ef431c31d5856961
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2022-11-03 12:56:05 +0530

    Update script-8.sh

--- script-8.sh
+++ script-8.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cat ${1}.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
+#cat ${1}.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
+PD='$1'
+cat ${PD}.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
