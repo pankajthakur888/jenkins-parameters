09963e0e8d976ab44917bfbf0890a120d183f149
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2022-11-03 12:10:17 +0530

    Update script-6.sh

--- script-6.sh
+++ script-6.sh
@@ -1,1 +1,4 @@
- x
+#!/bin/sh
+#Read
+##From Agent Master Parameters need to Create sh
+cat gcr.io-cadvisor-cadvisor.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
