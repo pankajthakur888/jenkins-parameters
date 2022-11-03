e25c7a81fb56bc0637d800e1e94e32d1bf9a606d
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2022-11-03 14:27:29 +0530

    Update script-8.sh

--- script-8.sh
+++ script-8.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #cat ${1}.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
-cat .gcr.io-cadvisor-cadvisor.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
+cat gcr.io-cadvisor-cadvisor.json | jq --compact-output '.[].tags' | sed 's/"//g' | sed "s/[][]//g" | sed 's/ \n //g' | sed '/^[[:space:]]*$/d'
